1ceeb40bdb8ffeeff52168305a49493b2c13efaa
hoangthe2924 <hoangthe2924@users.noreply.github.com>
2021-11-30 12:45:15 +0700

    fix link apis

--- components/classes/index.js
+++ components/classes/index.js
@@ -7,20 +7,20 @@ router.get("/", (req, res) => classController.findAll(req, res));
 
 router.post("/", (req, res) => classController.create(req, res));
 
-router.get("/detail/:id", (req, res) => classController.findOne(req, res));
-
-router.post("/detail/:id/addUser", (req, res) => classController.addUser(req, res));
-
 router.post("/people/invite", authTeacher, classController.invitePeople);
 
-router.post("/assignments",authTeacher, classController.createAssignment); //ok
+router.post("/assignments", authTeacher, classController.createAssignment);
 
 router.put("/assignments",authTeacher, classController.updateAssignment);
 
-router.delete("/detail/:classID/assignments/:assignmentID",authTeacher, classController.deleteAssignment);
-
-router.get("/detail/:classID/assignments/",authTeacher, classController.getListAssignment);
-
 router.put("/assignments/order",authTeacher, classController.updateAssignmentOrder);
+
+router.delete("/:classID/assignments/:assignmentID", authTeacher, classController.deleteAssignment);
+
+router.get("/:classID/assignments", authTeacher, classController.getListAssignment);
+
+router.post("/:id/user", (req, res) => classController.addUser(req, res));
+
+router.get("/:id", (req, res) => classController.findOne(req, res));
 
 module.exports = router;
