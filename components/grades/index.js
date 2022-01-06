6a262ca075ff09cbdcc5ce7cfebead27f4ccf21c
hoangthe2924 <hoangthe2924@users.noreply.github.com>
2022-01-06 14:30:16 +0700

    add new grades api for grade detail

--- components/grades/index.js
+++ components/grades/index.js
@@ -4,22 +4,12 @@ const gradeController = require("./grade.controller");
 const authTeacher = require("../../middleware/teacher.mdw");
 
 
-router.get(
-  "/:asssignmentID/studentGrades",
-  authTeacher,
-  gradeController.getStudentGrades
-);
+router.get("/:asssignmentID/studentGrades", authTeacher, gradeController.getStudentGrades);
 
-router.put(
-  "/:asssignmentID/studentGrades",
-  authTeacher,
-  gradeController.updateStudentGrades
-);
+router.put("/:asssignmentID/studentGrades", authTeacher, gradeController.updateStudentGrades);
 
-router.put(
-  "/:asssignmentID/finalize",
-  authTeacher,
-  gradeController.finalizeGrades
-);
+router.put("/:asssignmentID/finalize", authTeacher, gradeController.finalizeGrades);
+
+router.get("/studentGradeDetail", gradeController.getStudentGradeDetail);
 
 module.exports = router;
